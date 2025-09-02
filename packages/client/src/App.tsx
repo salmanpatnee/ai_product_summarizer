178f578cda21e709f6a11ffaa07a655b06a4df68
salmanpatnee <salmanpatni92@gmail.com>
2025-09-03 00:34:44 +0500

    Set up prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error("Error fetching message:", error));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message))
+         .catch((error) => console.error('Error fetching message:', error));
+   }, []);
 
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  );
+   return (
+      <div className="flex min-h-svh flex-col items-center justify-center">
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,13 +8,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!!');
+   res.send('Hello World!!');
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({ 'message': 'Hello from the API!' });
+   res.json({ message: 'Hello from the API!' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+   console.log(`Server is running at http://localhost:${port}`);
 });
