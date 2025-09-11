c1936de007d2d649e9fd43918413c4d48ac4c642
salmanpatnee <salmanpatni92@gmail.com>
2025-09-11 22:51:21 +0500

    Create API to fetch product reviews

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,28 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dontenv from 'dotenv';
+import { PrismaClient } from './generated/prisma';
 
 dontenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!!');
-});
+app.get('/api/products/:id/reviews', async (req: Request, res: Response) => {
+   const prisma = new PrismaClient()
+   const productId = Number(req.params.id)
 
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello from the API!' });
+   if (isNaN(productId)) {
+      res.status(400).json({ error: "Invalid product ID." })
+      return;
+   }
+
+   const reviews = await prisma.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: 'desc' }
+   })
+
+   res.json(reviews)
 });
 
 app.listen(port, () => {
