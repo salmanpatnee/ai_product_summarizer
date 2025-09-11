e21a20d9847906b92b4df8b66037732b4665e124
salmanpatnee <salmanpatni92@gmail.com>
2025-09-12 01:28:59 +0500

    Generate summaries

--- packages/server/services/review.service.ts
+++ packages/server/services/review.service.ts
@@ -1,5 +1,10 @@
+import OpenAI from "openai"
 import { type Review } from "../generated/prisma"
 import { reviewRepository } from "../repositories/review.repository"
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
 export const reviewService = {
     async fetchProductReviews(productId: number): Promise<Review[]> {
@@ -11,8 +16,18 @@ export const reviewService = {
 
         const joinedReviews = reviews.map(review => review.body).join('\n\n')
 
-        const summary = "This is a summary";
+        const prompt = `
+       Summarize the following customer reviews into a short paragraph. Highlight the main positive aspects customers liked, and the main negative aspects they mentioned. Keep the tone neutral and professional.
 
-        return summary;
+        Reviews: ${joinedReviews}
+        `
+        const response = await client.responses.create({
+            'model': 'gpt-4.1',
+            input: prompt,
+            temperature: 0.2,
+            max_output_tokens: 500,
+        })
+
+        return response.output_text;
     }
 }
