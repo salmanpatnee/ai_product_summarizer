64e28bb2f32786cfdf1703586efa74fe5390331a
salmanpatnee <salmanpatni92@gmail.com>
2025-09-13 00:02:05 +0500

    Handle edge cases

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
 import { reviewService } from '../services/review.service';
+import { productRepository } from '../repositories/product.repository';
+import { reviewRepository } from '../repositories/review.repository';
 export const reviewsController = {
     async fetchProductReviews(req: Request, res: Response) {
 
@@ -20,6 +22,20 @@ export const reviewsController = {
 
         if (isNaN(productId)) {
             res.status(400).json({ error: "Invalid product ID." })
+            return;
+        }
+
+        const product = await productRepository.getProduct(productId)
+
+        if (!product) {
+            res.status(400).json({ error: "Invalid product." })
+            return;
+        }
+
+        const review = await reviewRepository.fetchProductReviews(productId, 1)
+
+        if (!review.length) {
+            res.status(400).json({ error: "Ther are no reviews of this product." })
             return;
         }
 
